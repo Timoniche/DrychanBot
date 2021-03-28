24f159f74799201559861851693baa51979b702c
Dmitrii Dulaev <dmitriidulaev@gmail.com>
2021-03-28 22:58:25 +0300

    swagger

---
+++ README.md
@@ -0,0 +1,3 @@
+## Dating Bot For Drychan
+
+![alt text](https://github.com/Timoniche/DrychanBot/blob/main/logo.JPEG)
--- src/main/java/com/drychan/DrychanBotApplication.java
+++ src/main/java/com/drychan/DrychanBotApplication.java
@@ -2,8 +2,10 @@ package com.drychan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class DrychanBotApplication {
 
     public static void main(String[] args) {
