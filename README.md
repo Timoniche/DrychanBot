501f889b02430ffef864704b843a13cbe682798a
Dmitrii Dulaev <dmitriidulaev@gmail.com>
2021-04-02 01:54:00 +0300

    docker, docker-compose, postgres set up

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ## Dating Bot For Drychan
 
 ![alt text](https://github.com/Timoniche/DrychanBot/blob/main/logo.JPEG)
+
+before run:
+`mvn -N io.takari:maven:wrapper`
+
+rebuild.sh to re-run docker-compose
---
+++ rebuild.sh
@@ -0,0 +1,8 @@
+./mvnw clean package -DskipTests
+cp target/DrychanBot-1.0-SNAPSHOT.jar src/main/docker
+
+cd src/main/docker
+docker-compose down
+docker rmi drychan-bot:latest
+docker build -t drychan-bot .
+docker-compose up
