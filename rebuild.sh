88bcd1a8578236c1bab4757a463c14a139713e07
timoniche <dmitriidulaev@gmail.com>
2021-08-28 23:37:45 +0300

    jar for server

--- rebuild.sh
+++ rebuild.sh
@@ -2,6 +2,7 @@ mvn -N io.takari:maven:wrapper
 ./mvnw clean package -DskipTests
 cp target/DrychanBot-1.0-SNAPSHOT.jar src/main/docker
 
+# shellcheck disable=SC2164
 cd src/main/docker
 docker-compose down
 docker rmi drychan-bot:latest
