a478b3ace34e33230cbcf445e0974c73618ac6c4
timoniche <dmitriidulaev@gmail.com>
2021-09-27 22:11:11 +0300

    jar updated

--- scripts/build_maven_docker_and_run.sh
+++ scripts/build_maven_docker_and_run.sh
@@ -1,9 +1,10 @@
+cd ..
 mvn -N io.takari:maven:wrapper
 ./mvnw clean package -DskipTests
-cp target/DrychanBot-1.0-SNAPSHOT.jar ../src/main/docker
+cp target/DrychanBot-1.0-SNAPSHOT.jar src/main/docker
 
 # shellcheck disable=SC2164
-cd ../src/main/docker
+cd src/main/docker
 docker-compose down
 docker rmi drychan-bot:latest
 docker build -t drychan-bot .
