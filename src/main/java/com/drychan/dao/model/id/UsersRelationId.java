d75cd7ac9a905322ffc1ac225befa601e83bb589
timoniche <dmitriidulaev@gmail.com>
2021-10-17 17:15:29 +0300

    hashCode + equals for composite key

--- src/main/java/com/drychan/dao/model/id/UsersRelationId.java
+++ src/main/java/com/drychan/dao/model/id/UsersRelationId.java
@@ -1,6 +1,7 @@
 package com.drychan.dao.model.id;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -11,4 +12,21 @@ public class UsersRelationId implements Serializable {
     private Integer userId;
 
     private Integer userToId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        UsersRelationId that = (UsersRelationId) o;
+        return userId.equals(that.userId) && userToId.equals(that.userToId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, userToId);
+    }
 }
